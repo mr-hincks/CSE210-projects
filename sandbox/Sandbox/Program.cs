75a774bd747fdc40fcb335830df4a1baef3adeaf
mr-hincks <hinckleyoa93@gmail.com>
2025-03-14 19:10:23 +0000

    Changed the displayed message

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,6 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+    {  Console.WriteLine("Hello World!!!.");
     }
-}
+} 
