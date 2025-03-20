9073412b7fc6ff6676113c7ac2a182dc99364c17
mr-hincks <hinckleyoa93@gmail.com>
2025-03-20 22:38:17 +0000

    activity3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+    Random randomGenerator = new Random();
+    int magic = randomGenerator.Next(1, 101);
+    int guess = -1;
+    while(guess != magic)
+    {
+        Console.Write("What is your guess? ");
+        int number=int.Parse(Console.ReadLine());
+    
+    
+        if(number < magic )
+        {
+        Console.WriteLine("Higher");
+        }
+        else if(number > magic)
+        {
+        Console.WriteLine("Lower");
+        }
+        else
+        {
+        Console.WriteLine("You guessed it!");
+        break;
+        }     
+    
+    }
+    
+
+    
+    
+
+
+
     }
 }
