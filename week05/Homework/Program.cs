9969349b09fb751322d78b8677da9b46ee257a4c
mr-hincks <hinckleyoa93@gmail.com>
2025-04-16 13:42:07 +0000

    workingAssignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Hinckley Owusu Ansah","The Bond Of 1844");
+        Console.WriteLine(assignment1.GetSummary());
+        MathAssignment homework1 = new MathAssignment("Hinckley","Fraction", "2.3.1","8-19");
+        Console.WriteLine(homework1.GetSummary());
+        Console.WriteLine(homework1.GetHomeworkList());
+
+        //test writeassignment
+        WritingAssignment writingwork1 = new WritingAssignment("Stepehen Adjetey","Ghana History","The Sagranti War");
+        Console.WriteLine(writingwork1.GetSummary());
+        Console.WriteLine(writingwork1.GetWritingInformation());
     }
 }
